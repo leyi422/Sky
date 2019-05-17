242e1313e72f3d09370c852a4d2ddcdcbc2c1785
ZhangJiawei <zhangjiawei.cn@Hotmail.com>
2019-05-17 10:46:38 +0800

    Initial Commit

---
+++ Sky/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sky
+//
+//  Created by Jiawei Zhang on 2019/5/17.
+//  Copyright © 2019 ZhangJiawei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
